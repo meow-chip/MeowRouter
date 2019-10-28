9b37a22a6fdb4bac93000edd6159fdb4f25a2508
louchenyao <louchenyao@gmail.com>
2019-10-28 13:47:31 +0800

    NatTable with linear search

--- src/main/scala/nat/nat.scala
+++ src/main/scala/nat/nat.scala
@@ -2,7 +2,7 @@ package nat
 
 import chisel3._
 import chisel3.util._
-import data.{Packet, Status, PacType}
+import data.{Packet, Status, PacType, IP}
 
 class Nat(val PORT_COUNT: Int) extends Module {
   val io = IO(new Bundle {
@@ -34,7 +34,8 @@ class Nat(val PORT_COUNT: Int) extends Module {
   }
 
   // create a nat table
-  val natTable = VecInit(Array.fill[NatTableEntry](200){
+  val tableSize = 200
+  val natTable = VecInit(Array.fill[NatTableEntry](tableSize){
     val e = new NatTableEntry()
     e.valid := false.B
     e
@@ -43,10 +44,17 @@ class Nat(val PORT_COUNT: Int) extends Module {
   // variables for the state machine
   val sIDLE :: sMATCHING :: Nil = Enum(2)
   val state = RegInit(sIDLE)
+  // used for linear search
+  val searchingP = RegInit(0.U(log2Ceil(tableSize+1).W))
 
   // the working packet and its status
   val packet = Reg(new Packet(PORT_COUNT))
   val status = RegInit(Status.vacant)
+  // the packet details
+  val srcIP = packet.ip.src
+  val srcPort = packet.icmp.id
+  val dstIP = packet.ip.dest
+  val dstPort = packet.icmp.id
 
   // connecting output wires
   io.stall := state =/= sIDLE
@@ -59,12 +67,39 @@ class Nat(val PORT_COUNT: Int) extends Module {
         status := io.status
         packet := io.input
         when(io.status =/= Status.vacant) {
-          // TODO:
+          when (io.status =/= Status.dropped && io.input.eth.pactype === PacType.ipv4 && io.input.ip.proto === IP.ICMP_PROTO.U) {
+            state := sMATCHING
+            // TODO: Should we set the status to Status.vacant in case the next stage keep accepting packets? Or it is OK since we have io.stall = true?
+            status := Status.vacant
+            searchingP := 0.U
+          } .otherwise {
+            status := io.status
+          }
         }
       }
     }
     is (sMATCHING) {
-      // TODO:
+      val outbound = srcIP === natTable(searchingP).priIP && srcPort === natTable(searchingP).priPort && natTable(searchingP).valid
+      val inbound = dstIP === natTable(searchingP).bindIP && dstPort === natTable(searchingP).bindPort && natTable(searchingP).valid
+      when (outbound) {
+        srcIP := natTable(searchingP).bindIP
+        srcPort := natTable(searchingP).bindPort
+      } .elsewhen (inbound) {
+        dstIP := natTable(searchingP).priIP
+        dstPort := natTable(searchingP).priPort
+      } .otherwise {
+        when (searchingP + 1.U < tableSize.U) {
+          searchingP := searchingP + 1.U
+        } .otherwise {
+          state := sIDLE
+          status := Status.normal
+        }
+      }
+
+      when (outbound || inbound) {
+        state := sIDLE
+        status := Status.normal
+      }
     }
   }
 }
