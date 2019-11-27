39d2598e01bb3bfeaa7be642a63a3b3a0f4007e1
louchenyao <louchenyao@gmail.com>
2019-11-27 18:42:17 +0800

    more than one slots in a bucket

--- src/main/scala/cuckoo/cuckoo.scala
+++ src/main/scala/cuckoo/cuckoo.scala
@@ -5,6 +5,16 @@ import chisel3.util._
 import _root_.util.CRC
 
 class HashTable(val KeySize: Int, val ValueSize: Int) extends Module {
+  class KeyValuePair(val KeySize: Int, val ValueSize: Int) extends Bundle {
+    val key = UInt(KeySize.W)
+    val value = UInt(ValueSize.W)
+    val valid = Bool()
+  }
+
+  class Bucket() extends Bundle {
+    val pairs = Vec(4, new KeyValuePair(KeySize, ValueSize))
+  }
+
   val io = IO(new Bundle {
     val key = Input(UInt(KeySize.W))
     val value = Input(UInt(ValueSize.W))
@@ -15,44 +25,46 @@ class HashTable(val KeySize: Int, val ValueSize: Int) extends Module {
     val queryEn = Input(Bool())
 
     val answerValue = Output(UInt(ValueSize.W))
-    val answerEn = Output(Bool())
     val answerFound = Output(Bool())
+    val setSucc = Output(Bool()) // is set operation successful
     val stall = Output(Bool())
+    val dbg = Output(new Bucket())
+    val dbg_keyhash = Output(UInt(KeySize.W))
   });
 
-  class KeyValuePair(val KeySize: Int, val ValueSize: Int) extends Bundle {
-    val key = UInt(KeySize.W)
-    val value = UInt(ValueSize.W)
-    val valid = Bool()
-  }
-
-  val mem = Mem(1024, (new KeyValuePair(KeySize, ValueSize)).cloneType)
-  val sIDLE :: sReading :: sComparing :: sSetting :: Nil = Enum(4)
+  val mem = Mem(1024, new Bucket())
+  val sIDLE :: sReading :: sComparing :: sSetting :: sSetting1 :: sSetting2 :: sSetting3 :: Nil = Enum(7)
   val state = RegInit(sIDLE)
   
   val key = Reg(UInt(KeySize.W)) // querying on key
   val keyHash = Reg(UInt(log2Ceil(1024).W))
-  val readPair = Reg(new KeyValuePair(KeySize, ValueSize))
+  val bucket = Reg(new Bucket())
+  val p = RegInit(0.asUInt(8.W))
+  val pair = Reg(new KeyValuePair(KeySize, ValueSize))
   val found = RegInit(false.B)
-  val answerEn = RegInit(false.B)
+  val setSucc = RegInit(false.B)
 
   // connecting io wires
-  io.answerValue := readPair.value
-  io.answerEn := answerEn
-  io.answerFound := readPair.key === key && readPair.valid
+  io.answerValue := pair.value
+  io.answerFound := found
+  io.setSucc := setSucc
   io.stall := state =/= sIDLE
+  io.dbg := bucket
+  io.dbg_keyhash := keyHash
 
   switch (state) {
     is (sIDLE) {
-      answerEn := false.B
       key := io.key
       keyHash := CRC(CRC.CRC_8F_6, io.key, KeySize)
+      setSucc := false.B
+      found := false.B
+      p := 0.U
       when (io.queryEn) {
         state := sReading
       } .elsewhen (io.setEn) {
-        readPair.key := io.key
-        readPair.value := io.value
-        readPair.valid := true.B
+        pair.key := io.key
+        pair.value := io.value
+        pair.valid := true.B
         state := sSetting
       }
     }
@@ -61,11 +73,16 @@ class HashTable(val KeySize: Int, val ValueSize: Int) extends Module {
     // * the states for query
     // *******
     is (sReading) {
-      readPair := mem.read(keyHash)
+      bucket := mem.read(keyHash)
       state := sComparing
     }
     is (sComparing) {
-      answerEn := true.B
+      for (i <- 0 until 4) {
+        when (bucket.pairs(i).key === key) {
+          found := true.B
+          pair := bucket.pairs(i)
+        }
+      }
       state := sIDLE
     }
 
@@ -73,7 +90,27 @@ class HashTable(val KeySize: Int, val ValueSize: Int) extends Module {
     // * the state for insert
     // *******
     is (sSetting) {
-      mem.write(keyHash, readPair)
+      bucket := mem.read(keyHash)
+      state := sSetting1
+    }
+    is (sSetting1) {
+      // !!! This doesn't work. The behavior is that when using the second slot (pairs(1)), it will set all slots to pair, i.e. firstEmptys are all true.
+      val firstEmptys = Array.tabulate(4) { bucket.pairs(_).valid === false.B }
+      for (i <- 1 until 4) {
+        for (j <- 0 until i) {
+          firstEmptys(i) = firstEmptys(i) && bucket.pairs(j).valid
+        }
+      }
+      for (i <- 0 until 4) {
+        when (firstEmptys(i)) {
+          bucket.pairs(i) := pair
+          setSucc := true.B
+        }
+      }
+      state := sSetting2
+    }
+    is (sSetting2) {
+      mem.write(keyHash, bucket)
       state := sIDLE
     }
   }
--- src/test/scala/cuckoo.scala
+++ src/test/scala/cuckoo.scala
@@ -5,12 +5,13 @@ import chisel3.util._
 import chisel3.iotesters.{Driver, TesterOptionsManager, PeekPokeTester}
 
 class TestCuckoo(c: HashTable, KeySize: Int, ValueSize: Int) extends PeekPokeTester(c) {
-  val keys = List(1, 1024+1, 2048+1, 65536+1, 890)
-  val vals = List(1, 2, 3, 233, 4)
+  // gerenate keys and val
+  // TODO: When the #keys is larger than the 250, the hash table may fail
+  val keys = for (i <- 0 until 200) yield scala.util.Random.nextInt(2047483647)
+  val vals = for (i <- 0 until 200) yield scala.util.Random.nextInt(255)
 
   def waitWhenStalled(): Unit = {
     var cnt = 0
-    step(1)
     while (peek(c.io.stall) == BigInt(1)) {      
       cnt = cnt + 1
       assert(cnt <= 10)
@@ -21,23 +22,27 @@ class TestCuckoo(c: HashTable, KeySize: Int, ValueSize: Int) extends PeekPokeTes
 
   // insert
   for (i <- 0 until keys.length) {
-    waitWhenStalled()
     poke(c.io.key, keys(i))
     poke(c.io.value, vals(i))
     poke(c.io.setEn, true)
     poke(c.io.queryEn, false)
+    step(1)
     waitWhenStalled()
+    println("i = " + i + " key_hash = " + peek(c.io.dbg_keyhash) + " key = " + keys(i))
+    // for (j <- 0 until 4) {
+    //   println("key = " + peek(c.io.dbg.pairs(j).key) + ", value = " + peek(c.io.dbg.pairs(j).value) + ", valid = " + peek(c.io.dbg.pairs(j).valid))
+    // }
+    assert(peek(c.io.setSucc) == BigInt(1))
   }
 
   // query
   for (i <- 0 until keys.length) {
-    waitWhenStalled()
     poke(c.io.key, keys(i))
     poke(c.io.setEn, false)
     poke(c.io.queryEn, true)
+    step(1)
     waitWhenStalled()
     //println(peek(c.io.answerValue).toString)
-    assert(peek(c.io.answerEn) == BigInt(1))
     assert(peek(c.io.answerFound) == BigInt(1))
     assert(peek(c.io.answerValue) == BigInt(vals(i)))
   }
